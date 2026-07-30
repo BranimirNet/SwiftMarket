152b17df22146d1490bc7003f54c977ed823cc8a
BranimirNet <branimir.netrebasic@gmail.com>
2026-07-30 18:35:29 +0200

    Fixed database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\ProductSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,15 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory()->create([
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-    ]);
-
-    $this->call([
-        CategorySeeder::class,
-        ProductSeeder::class,
-        ImageSeeder::class,
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
